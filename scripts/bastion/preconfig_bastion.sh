72a2d0187f919f307e1344a6c086ae78efdfac9c
irislee721 <y0022078@gmail.com>
2025-04-17 02:28:48 +0800

    Update preconfig_bastion.sh

--- scripts/bastion/preconfig_bastion.sh
+++ scripts/bastion/preconfig_bastion.sh
@@ -1,9 +1,22 @@
-# /bin/bash
+#!/bin/bash
 
-# TODO 參數化
+# 清除前四個參數的環境變數
+unset $1
+unset $2
+unset $3
+unset $4
 
+# 設定參數（可以在執行腳本時傳入）
+export BASTION_IP=$1
+export TARBALL_PATH=$(echo "${2:-/root/install_source/ansible-navigator-rpm-9.4-min.tar}")
+export TAR_DEST_PATH=$(echo "${3:-/root/install_source/ansible-navigator-rpm-9.4}")
+export EE_IMAGE_TAR=$(echo "${4:-eeimage}")
+export EE_IMAGE_NAME=$(basename "$EE_IMAGE_TAR" .tar)
+
+# Mount DVD/image 掛光碟到機器上
 mount /dev/sr0 /mnt
 
+# 建立 yum repository
 cat << EOF > /etc/yum.repos.d/dvd.repo
 [BaseOS]
 name=BaseOS
@@ -19,17 +32,22 @@ gpgcheck=0
 EOF
 
 # 解開所有準備好的 tar 包
-tar xvf ansible-navigator-rpm-9.4-min.tar -C  ['解 tar 之路徑']
+mkdir -p ${TAR_DEST_PATH}
+tar xvf ${TARBALL_PATH} -C ${TAR_DEST_PATH}
 
 # 安裝所有 rpm 包
-yum localinstall ['解 tar 之路徑']/* --allowerasing
+yum localinstall -y ${TAR_DEST_PATH}/* --allowerasing
 
 # 於 bastion 產生 ssh-key，並設定免密登入
-ssh-keygen
-ssh-copy-id root@['bastion ip']
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa <<< y
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
 
 # 從 tar 檔中載入容器映像檔到 Podman 的本地鏡像庫
-podman load -i ['包起來的 ee tar 檔名稱'].tar
+podman load -i ${EE_IMAGE_TAR}
 
 # 使用 ansible 運行自動化設定配置腳本
-ansible-navigator run --eei ${EE_IMAGE_NAME}:${VERSION_DATE} -i ../../roles/inventory -mstdout ../../roles/install.yml
+ansible-navigator run \
+  --eei ${EE_IMAGE_NAME} \
+  -i ../../roles/inventory \
+  -m stdout ../../roles/install.yml
