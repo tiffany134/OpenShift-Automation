6315cfcb019924f68e8789aaa7f2b4048399ca60
molasun <mola79321@gmail.com>
2025-04-22 15:25:32 +0800

    Update prep_script.sh

--- scripts/prep_script.sh
+++ scripts/prep_script.sh
@@ -20,16 +20,16 @@ done < "$config_file"
 
 # 主程式
 main(){
-  # build_ee_image
+  build_ee_image
   download_ansible
-  # get_tool
+  get_tool
 }
 
 
 # 創建自動化的 ee 鏡像並封裝成 tar 檔
 build_ee_image(){
   if [[ "$CUSTOM_EE" = "true" ]]; then
-    echo "創建客製化 ee..."
+    echo "創建客製化 ee 並打包..."
 
     # 登入 registry.redhat.io
     mkdir ~/.docker
@@ -47,6 +47,7 @@ build_ee_image(){
     # 將 ee image 包成 tar 檔
     podman save -o ${EE_IMAGE_NAME}-${VERSION_DATE}.tar ${EE_IMAGE_NAME}
   else
+    echo "下載 ee 鏡像並打包..."
 
     # TODO 使用 podman login and pull ee image
     
@@ -56,7 +57,6 @@ build_ee_image(){
 }
 
 download_ansible(){
-
   # 下載 AAP rpm
   echo "開始下載 AAP rpm..."
   dnf install --enablerepo="${AAP_REPO}" --downloadonly --installroot=/root/rpm/rootdir --downloaddir="${AAP_DIR}" --releasever="${RHEL_MINOR_VERSION}" ansible-navigator
@@ -68,23 +68,31 @@ download_ansible(){
 
 # 下載安裝所需工具
 get_tool(){
+  echo "下載安裝工具..."
+
   # 下載 openshift client
   wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OCP_RELEASE}/openshift-client-linux-${ARCHITECTURE}-${RHEL_VERSION}-${OCP_RELEASE}.tar.gz
+  echo "oc client 下載完成"
 
   # 下載 openshift install
   wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OCP_RELEASE}/openshift-install-${RHEL_VERSION}-${ARCHITECTURE}.tar.gz
+  echo "oc install 下載完成"
 
   # 下載 oc mirror
   wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OCP_RELEASE}/oc-mirror.${RHEL_VERSION}.tar.gz
+  echo "oc mirror 下載完成"
 
   # 下載 butane
   wget https://mirror.openshift.com/pub/openshift-v4/clients/butane/latest/butane-${ARCHITECTURE}
+  echo "butane 下載完成"
 
   # 下載 latest helm
   wget https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/helm/${HELM_VERSION}/helm-linux-${ARCHITECTURE}.tar.gz
+  echo "butane 下載完成"
 
   # 下載 latest mirror registry
   wget https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/mirror-registry/${MIRROR_REGISTRY_VERSION}/mirror-registry.tar.gz
+  echo "mirror registry 下載完成"
 }
 
 main
