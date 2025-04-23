4f5e80975f5e11f6a35903ffe9ed359ffff2e56b
molasun <mola79321@gmail.com>
2025-04-23 17:56:13 +0800

    Update prep_config.sh

--- scripts/prep_script.sh
+++ scripts/prep_script.sh
@@ -21,20 +21,53 @@ done < "$config_file"
 # 主程式
 main(){
   env_prep
+  git_clone
   build_ee_image
   download_ansible
   get_tool
   untar_oc_mirror
 }
 
+# 準備基本環境資訊
 env_prep(){
 
-  # 創建最終安裝檔案擺放位置
-  mkdir /root/install_file
+  dnf install --enablerepo=${AAP_REPO} ansible-navigator
 
-  # 將 pull secret 匯入到 config.json
-  mkdir /root/.docker
+  # 定義需要檢查/創建的目錄列表（可以自行替換或擴展）
+  CREATE_DIRS=(
+    "/root/install_file"
+    "/root/.docker"
+    "/root/install/ocp418"
+  )
+
+  # 使用迴圈創建所有目錄
+  for dir in "${CREATE_DIRS[@]}"; do
+    if [ -d "$dir" ]; then
+        echo "目錄 $dir 已存在，跳過創建"
+    else
+        echo "目錄 $dir 不存在，正在創建..."
+        mkdir -p "$dir"
+        # 檢查 mkdir 是否成功
+        if [ $? -eq 0 ]; then
+            echo "創建成功"
+        else
+            echo "創建失敗" > &2  # 將錯誤輸出到 stderr
+            exit 1                # 失敗時退出腳本
+        fi
+    fi
+  done
+    
+  # 將 pull-secret 匯到 config.json
   cat /root/pull-secret | jq > /root/.docker/config.json
+}
+
+# 拉取自動化所需 repo
+git_clone(){
+
+  git clone https://github.com/CCChou/ocp_bastion_installer.git ${OCP_INSTALLER_DIR}
+
+  git clone https://github.com/CCChou/OpenShift-EaaS-Practice.git ${GITOPS_DIR}
+
 }
 
 # 創建自動化的 ee 鏡像並封裝成 tar 檔
@@ -44,12 +77,9 @@ build_ee_image(){
       
     # 創建 ee image 創建路徑
     mkdir ${EE_DIR} && cd ${EE_DIR}
-  
-    # 取得 ee.yaml 範本
-    wget https://raw.githubusercontent.com/CCChou/OpenShift-Automation/refs/heads/main/ansible/execution-environment.yml
-  
+      
     # 創建 ee image
-    ansible-builder build -v3 -f execution-environment.yml -t ${EE_IMAGE_NAME}
+    ansible-builder build -v3 -f ${EE_YAML_PATH} -t ${EE_IMAGE_NAME}
   
     # 將 ee image 包成 tar 檔
     podman save -o /root/install_file/${EE_IMAGE_NAME}-${VERSION_DATE}.tar ${EE_IMAGE_NAME}
@@ -79,27 +109,27 @@ get_tool(){
   echo "下載安裝工具..."
 
   # 下載 openshift client
-  wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OCP_RELEASE}/openshift-client-linux-${ARCHITECTURE}-${RHEL_VERSION}-${OCP_RELEASE}.tar.gz > /root/install_file
+  wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OCP_RELEASE}/openshift-client-linux-${ARCHITECTURE}-${RHEL_VERSION}-${OCP_RELEASE}.tar.gz -P /root/install_file
   echo "oc client 下載完成"
 
   # 下載 openshift install
-  wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OCP_RELEASE}/openshift-install-${RHEL_VERSION}-${ARCHITECTURE}.tar.gz > /root/install_file
+  wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OCP_RELEASE}/openshift-install-${RHEL_VERSION}-${ARCHITECTURE}.tar.gz -P /root/install_file
   echo "oc install 下載完成"
 
   # 下載 oc mirror
-  wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OCP_RELEASE}/oc-mirror.${RHEL_VERSION}.tar.gz > /root/install_file
+  wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OCP_RELEASE}/oc-mirror.${RHEL_VERSION}.tar.gz -P /root/install_file
   echo "oc mirror 下載完成"
 
   # 下載 butane
-  wget https://mirror.openshift.com/pub/openshift-v4/clients/butane/latest/butane-${ARCHITECTURE} > /root/install_file
+  wget https://mirror.openshift.com/pub/openshift-v4/clients/butane/latest/butane-${ARCHITECTURE} -P /root/install_file
   echo "butane 下載完成"
 
   # 下載 latest helm
-  wget https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/helm/${HELM_VERSION}/helm-linux-${ARCHITECTURE}.tar.gz > /root/install_file
+  wget https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/helm/${HELM_VERSION}/helm-linux-${ARCHITECTURE}.tar.gz -P /root/install_file
   echo "butane 下載完成"
 
   # 下載 latest mirror registry
-  wget https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/mirror-registry/${MIRROR_REGISTRY_VERSION}/mirror-registry.tar.gz > /root/install_file
+  wget https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/mirror-registry/${MIRROR_REGISTRY_VERSION}/mirror-registry.tar.gz -P /root/install_file
   echo "mirror registry 下載完成"
 }
 
@@ -110,7 +140,7 @@ untar_oc_mirror(){
 
   chmod a+x /usr/local/bin/oc-mirror
 
+  cp /root/Openshift-Automation/yaml/imageset-config.yaml /root/install/ocp418
 }
-
 
 main
