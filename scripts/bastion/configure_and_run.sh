c9a907abe278ed04531c154c189739cb80cb4591
irislee721 <59044523+irislee721@users.noreply.github.com>
2025-04-24 02:09:09 +0800

    Update configure_and_run.sh

--- scripts/bastion/configure_and_run.sh
+++ scripts/bastion/configure_and_run.sh
@@ -6,17 +6,23 @@ unset $2
 unset $3
 
 # 設定參數（可以在執行腳本時傳入）
-export TARBALL_PATH=$(echo "${1:-/root/install_source/ansible-navigator-rpm-9.4-min.tar}")
-export TAR_DEST_PATH=$(echo "${2:-/root/install_source/ansible-navigator-rpm-9.4}")
-export EE_IMAGE_TAR=$3
-export EE_IMAGE_NAME=$(basename "$EE_IMAGE_TAR".tar)
+export TARBALL_PATH="${1:-/root/install_source/ansible-navigator-rpm-9.4-min.tar}"
+export TAR_DEST_PATH="${2:-/root/install_source/ansible-navigator-rpm-9.4}"
+export EE_IMAGE_TAR="${3:-/root/install_source/eeimage-v1.tar}"
+export EE_IMAGE_NAME="${4:-quay.io/rhtw/ee-bas-auto:v1.0}"
+
+# 確認參數
+echo "[INFO] TARBALL_PATH   = $TARBALL_PATH"
+echo "[INFO] TAR_DEST_PATH  = $TAR_DEST_PATH"
+echo "[INFO] EE_IMAGE_TAR   = $EE_IMAGE_TAR"
+echo "[INFO] EE_IMAGE_NAME  = $EE_IMAGE_NAME"
 
 # 解開所有準備好的 tar 包
 mkdir -p ${TAR_DEST_PATH}
-tar xvf ${TARBALL_PATH} -C ${TAR_DEST_PATH}
+tar xvf ${TARBALL_PATH} -C ${TAR_DEST_PATH} --strip-components=1
 
 # 安裝所有 rpm 包
-yum localinstall -y ${TAR_DEST_PATH}/* --allowerasing
+yum localinstall -y ${TAR_DEST_PATH}/* --allowerasing --skip-broken
 
 # 於 bastion 產生 ssh-key，並設定免密登入
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa <<< y
@@ -27,7 +33,4 @@ chmod 600 ~/.ssh/authorized_keys
 podman load -i ${EE_IMAGE_TAR}
 
 # 使用 ansible 運行自動化設定配置腳本
-ansible-navigator run \
-  --eei ${EE_IMAGE_NAME} \
-  -i ../../roles/inventory \
-  -m stdout ../../roles/install.yml
+ansible-navigator run --eei ${EE_IMAGE_NAME} --pp missing -i /root/OpenShift-Automation/roles/inventory -m stdout /root/OpenShift-Automation/roles/install.yml
